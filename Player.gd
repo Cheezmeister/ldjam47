9473cad9689a6be54400a28c4e9334c47ab786a0
Cheezmeister <github@luchenlabs.com>
2020-10-03 16:24:47 -0400

    Speed up piggy

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var MOVE_SPEED: float = 5
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,7 +9,6 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hooray")
 	pass # Replace with function body.
 
 
@@ -18,13 +18,13 @@ var count: int = 0
 func _process(delta):
 
 	if (Input.is_action_pressed("ui_up")):
-		self.transform.origin.y -= 1
+		self.transform.origin.y -= MOVE_SPEED
 	if (Input.is_action_pressed("ui_down")):
-		self.transform.origin.y += 1
+		self.transform.origin.y += MOVE_SPEED
 	if (Input.is_action_pressed("ui_left")):
-		self.transform.origin.x -= 1
+		self.transform.origin.x -= MOVE_SPEED
 	if (Input.is_action_pressed("ui_right")):
-		self.transform.origin.x += 1
+		self.transform.origin.x += MOVE_SPEED
 	if (Input.is_key_pressed(KEY_ESCAPE)):
 		get_tree().quit()
 	return delta
